4337f34aa4dc84883897138f84bc071ce8692f97
srogoz <sarah.rogoz@hotmail.com>
2025-12-12 14:15:11 +0100

    cleaned up

--- antrax_mask_generation.py
+++ antrax_mask_generation.py
@@ -7,152 +7,11 @@ Created on Tue Nov 25 13:19:06 2025
 """
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import json
-############
-## get files with already done backgrounds
-os.chdir('/home/srogoz@ice.mpg.de/ulr-lab/Users/Sarah_saro7615/EXP2/')
-#force to convert to one channel
-bg_path = 'sarah_16bba_2_A_data/b16-3/parameters/backgrounds/background.png'
-mask_path = 'sarah_16bba_2_A_data/b16-3/parameters/masks/'
-background = cv2.imread(bg_path)
-
-
-####################
-#%%
-    
-import cv2
-import numpy as np
-colony_name="b16-3"
-# GLOBALS for simplicity
-point = None
-dragging = False
-radius = 6        # how close the mouse must be to "grab" the point
-##
-#visible radius for exp2: cam2: 350:
-    #rscale: 
-visible_radius=350
-
-def mouse_callback(event, x, y, flags, param):
-    global point, dragging
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # If point exists, check if click is near it
-        if point is not None:
-            px, py = point
-            if (x - px)**2 + (y - py)**2 <= radius**2:
-                dragging = True
-        else:
-            # Create new point
-            point = (x, y)
-
-    elif event == cv2.EVENT_MOUSEMOVE and dragging:
-        # Move point with mouse
-        point = (x, y)
-
-    elif event == cv2.EVENT_LBUTTONUP:
-        dragging = False
-
-
-def get_colonyarea(background, colony_name):
-    global point, dragging
-    point = None
-    dragging = False
-
-    img = background.copy()
-    cv2.namedWindow(colony_name)
-    cv2.setMouseCallback(colony_name, mouse_callback)
-
-    while True:
-        frame = img.copy()
-
-        # Draw draggable point
-        if point is not None:
-            cv2.circle(frame, point, visible_radius, (0, 255, 0), -1)
-
-        cv2.imshow(colony_name, frame)
-
-        key = cv2.waitKey(20)
-        if key == 13:  # Enter to confirm
-            break
-        if key == 27:  # ESC to cancel
-            point = None
-            break
-
-    cv2.destroyWindow(colony_name)
-    return point
-
 ################
-#%%
-point_scaled=get_colonyarea(background, colony_name, display_scale)
-################
-#%%rescale image to see all colonies
-#display_scale = 0.5  # e.g., 0.3–0.7 depending on your screen
-#img_display = cv2.resize(img, None, fx=display_scale, fy=display_scale)
-
-
-point = None
-dragging = False
-radius = 6        # how close the mouse must be to "grab" the point
-##
-#visible radius for exp2: cam2: 350:
-    #rscale: 
-visible_radius=350
-
-def mouse_callback(event, x, y, flags, param):
-    global point, dragging
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # If point exists, check if click is near it
-        if point is not None:
-            px, py = point
-            if (x - px)**2 + (y - py)**2 <= radius**2:
-                dragging = True
-        else:
-            # Create new point
-            point = (x, y)
-
-    elif event == cv2.EVENT_MOUSEMOVE and dragging:
-        # Move point with mouse
-        point = (x, y)
-
-    elif event == cv2.EVENT_LBUTTONUP:
-        dragging = False
-
-
-def get_colonyarea(background, colony_name):
-    global point, dragging
-    point = None
-    dragging = False
-
-
-    img = background.copy()
-    cv2.namedWindow(colony_name)
-    cv2.setMouseCallback(colony_name, mouse_callback)
-
-    while True:
-        frame = img.copy()
-
-        # Draw draggable point
-        if point is not None:
-            cv2.circle(frame, point, visible_radius, (0, 255, 0), -1)
-
-        cv2.imshow(colony_name, frame)
-
-        key = cv2.waitKey(20)
-        if key == 13:  # Enter to confirm
-            break
-        if key == 27:  # ESC to cancel
-            point = None
-            break
-
-    cv2.destroyWindow(colony_name)
-    return point
-
-################
-#%%collect inf background paths:
+#%%collect  EXP1 inf background paths:
 os.chdir('/home/srogoz@ice.mpg.de/ulr-lab/Users/Sarah_saro7615/EXP1_GROUPSIZEandCOMPOSITION/')
 #list of bg_path_list of missing colony areas in EXP1
 bg_path_list= { "a16-7":'sarah_3216ab_6_inf_20240626_161959/a16-7/parameters/backgrounds/background.png',
@@ -165,6 +24,7 @@ bg_path_list= { "a16-7":'sarah_3216ab_6_inf_20240626_161959/a16-7/parameters/bac
                 "ba16-6": 'sarah_3216ab_5_inf_20240626_161922/ba16-6/parameters/backgrounds/background.png'
                     
    }
+
     
 mask_path_list= { "a16-7":'sarah_3216ab_6_inf_20240626_161959/a16-7/parameters/masks/',
                 "a16-8": 'sarah_3216ab_6_inf_20240626_161959/a16-8/parameters/masks/',
@@ -189,8 +49,44 @@ json_path_list= { "a16-7":'sarah_3216ab_6_inf_20240626_161959/a16-7/parameters/p
    }
 
 ########
+#%% EXP2 timeframe A paths!!!!
+
+os.chdir('/home/srogoz@ice.mpg.de/ulr-lab/Users/Sarah_saro7615/EXP2/')
+#list of bg_path_list of missing colony areas in EXP1
+bg_path_list= { #cam3:
+                "bA16-3":'sarah_16bba_3_A_data/bA16-3/parameters/backgrounds/background.png',
+                "b16-4":'sarah_16bba_3_A_data/b16-4/parameters/backgrounds/background.png',
+                "B16-4":'sarah_16bba_3_A_data/bigb16-4/parameters/backgrounds/background.png',
+                "bB16-4": 'sarah_16bba_3_A_data/bB16-4/parameters/backgrounds/background.png',
+                "bA16-4": 'sarah_16bba_3_A_data/bA16-4/parameters/backgrounds/background.png',
+                "b16-5": 'sarah_16bba_3_A_data/b16-5/parameters/backgrounds/background.png'
+                  
+   }
+    
+mask_path_list= { #cam3:
+                "bA16-3":'sarah_16bba_3_A_data/bA16-3/parameters/masks/',
+                "b16-4":'sarah_16bba_3_A_data/b16-4/parameters/masks/',
+                "B16-4":'sarah_16bba_3_A_data/bigb16-4/parameters/masks/',
+                "bB16-4": 'sarah_16bba_3_A_data/bB16-4/parameters/masks/',
+                "bA16-4": 'sarah_16bba_3_A_data/bA16-4/parameters/masks/',
+                "b16-5": 'sarah_16bba_3_A_data/b16-5/parameters/masks/'
+                  
+   }
+
+json_path_list= { #cam3:
+                "bA16-3":'sarah_16bba_3_A_data/bA16-3/parameters/prmtrs.json',
+                "b16-4":'sarah_16bba_3_A_data/b16-4/parameters/prmtrs.json',
+                "B16-4":'sarah_16bba_3_A_data/bigb16-4/parameters/prmtrs.json',
+                "bB16-4": 'sarah_16bba_3_A_data/bB16-4/parameters/prmtrs.json',
+                "bA16-4": 'sarah_16bba_3_A_data/bA16-4/parameters/prmtrs.json',
+                "b16-5": 'sarah_16bba_3_A_data/b16-5/parameters/prmtrs.json'
+                  
+   }
+
+
+########
 #%% pick colony 
-colony_name="a16-8"
+colony_name="B16-4"
 
 bg_path=bg_path_list[colony_name] 
 mask_path = mask_path_list[colony_name]
@@ -216,6 +112,7 @@ scaled_radius=345
 visible_radius=int(round(scaled_radius*display_scale,0))
 
 ########################
+#FUNCTIONS
 #%%rescale background image to see all colonies: updated!
 
 
@@ -331,11 +228,6 @@ get_masks(scaled_radius, point_scaled, mask_path, bg_path)
 
 ###########
 #%% save parameters in prmtrs.json
-
-
-
-
-
 def update_prmtrs_jsn(json_path, center, visible_radius, vertices, r_scale, out_path=None):
     
     
@@ -366,4 +258,7 @@ def update_prmtrs_jsn(json_path, center, visible_radius, vertices, r_scale, out_
     
 ##############
 #%%
+point_scaled=get_colonyarea(bg_path, colony_name, display_scale)
+vertices, r_scale = get_vertices(point_scaled, scaled_radius, 136)
+get_masks(scaled_radius, point_scaled, mask_path, bg_path)
 update_prmtrs_jsn(json_path, point_scaled, scaled_radius, vertices, r_scale)
